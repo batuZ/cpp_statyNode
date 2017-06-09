dc111956d1e06f5701f02c40d3458784ec3997cd
batuZ <304701204@qq.com>
2017-06-10 00:02:25 +0800

    11

--- 链表_List_vector/main.cpp
+++ 链表_List_vector/main.cpp
@@ -7,33 +7,33 @@ using namespace std;
 /*
 Lists将元素按顺序储存在链表中. 与 向量(vectors)相比, 它允许快速的插入和删除，但是随机访问却比较慢.
 
-assign() 给list赋值 
-back() 返回最后一个元素 
-begin() 返回指向第一个元素的迭代器 
-clear() 删除所有元素 
-empty() 如果list是空的则返回true 
-end() 返回末尾的迭代器 
-erase() 删除一个元素 
-front() 返回第一个元素 
+assign()		给list赋值 
+back()			返回最后一个元素 
+begin()			返回指向第一个元素的迭代器 
+clear()			删除所有元素 
+empty()			如果list是空的则返回true 
+end()			返回末尾的迭代器 
+erase()			删除一个元素 
+front()			返回第一个元素 
 get_allocator() 返回list的配置器 
-insert() 插入一个元素到list中 
-max_size() 返回list能容纳的最大元素数量 
-merge() 合并两个list 
-pop_back() 删除最后一个元素 
-pop_front() 删除第一个元素 
-push_back() 在list的末尾添加一个元素 
-push_front() 在list的头部添加一个元素 
-rbegin() 返回指向第一个元素的逆向迭代器 
-remove() 从list删除元素 
-remove_if() 按指定条件删除元素 
-rend() 指向list末尾的逆向迭代器 
-resize() 改变list的大小 
-reverse() 把list的元素倒转 
-size() 返回list中的元素个数 
-sort() 给list排序 
-splice() 合并两个list 
-swap() 交换两个list 
-unique() 删除list中重复的元素
+insert()		插入一个元素到list中 
+max_size()		返回list能容纳的最大元素数量 
+merge()			合并两个list 
+pop_back()		删除最后一个元素 
+pop_front()		删除第一个元素 
+push_back()		在list的末尾添加一个元素 
+push_front()	在list的头部添加一个元素 
+rbegin()		返回指向第一个元素的逆向迭代器 
+remove()		从list删除元素 
+remove_if()		按指定条件删除元素 
+rend()			指向list末尾的逆向迭代器 
+resize()		改变list的大小 
+reverse()		把list的元素倒转 
+size()			返回list中的元素个数 
+sort()			给list排序 
+splice()		合并两个list 
+swap()			交换两个list 
+unique()		删除list中重复的元素
 */
 typedef list<double> MyList;
 void testList() {
@@ -64,24 +64,25 @@ void testList() {
 
 /*
 相当于自动大小的float[]
+
 1.push_back   在数组的最后添加一个数据
 2.pop_back    去掉数组的最后一个数据 
-3.at                得到编号位置的数据
-4.begin           得到数组头的指针
-5.end             得到数组的最后一个单元+1的指针
-6．front        得到数组头的引用
-7.back            得到数组的最后一个单元的引用
-8.max_size     得到vector最大可以是多大
-9.capacity       当前vector分配的大小
-10.size           当前使用数据的大小
-11.resize         改变当前使用数据的大小，如果它比当前使用的大，者填充默认值
-12.reserve      改变当前vecotr所分配空间的大小
-13.erase         删除指针指向的数据项
-14.clear          清空当前的vector
-15.rbegin        将vector反转后的开始指针返回(其实就是原来的end-1)
-16.rend          将vector反转构的结束指针返回(其实就是原来的begin-1)
-17.empty        判断vector是否为空
-18.swap         与另一个vector交换数据
+3.at          得到编号位置的数据
+4.begin       得到数组头的指针
+5.end         得到数组的最后一个单元+1的指针
+6.front       得到数组头的引用
+7.back        得到数组的最后一个单元的引用
+8.max_size    得到vector最大可以是多大
+9.capacity    当前vector分配的大小
+10.size       当前使用数据的大小
+11.resize     改变当前使用数据的大小，如果它比当前使用的大，者填充默认值
+12.reserve    改变当前vecotr所分配空间的大小
+13.erase      删除指针指向的数据项
+14.clear      清空当前的vector
+15.rbegin     将vector反转后的开始指针返回(其实就是原来的end-1)
+16.rend       将vector反转构的结束指针返回(其实就是原来的begin-1)
+17.empty      判断vector是否为空
+18.swap       与另一个vector交换数据
 */
 typedef vector<float> MyVec;
 void testVector() {
@@ -89,7 +90,7 @@ void testVector() {
 	c.clear();						//移除容器中所有数据。
 	c.empty();						//判断容器是否为空。
 	c.erase(c.end());				// 删除pos位置的数据
-	c.erase(c.begin(), c.end ());//删除[beg, end)区间的数据
+	c.erase(c.begin(), c.end ());	//删除[beg, end)区间的数据
 	c.front();						//传回第一个数据。
 	c.insert(c.begin(), 3.1f);		//在pos位置插入一个elem拷贝
 	c.pop_back();					//删除最后一个数据。
